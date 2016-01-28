f8b0c0862cb9faa67a78dad2fd486857c726f9cd
niklomi <nikloomi@gmail.com>
2016-01-29 00:53:00 +0300

    feedback edit + slack phtot

--- server/twitter/twitter.js
+++ server/twitter/twitter.js
@@ -5,19 +5,19 @@ if (inProduction()){
 	};
 
 	var favorite_words = '@tech_fights OR #tech_fights',
-	query_word = 'IT companies';
+	query_word = 'IT companies OR tech wars';
 
 	SyncedCron.add({
 		name: 'Twi_make_favorite',
 		schedule: function(parser) {
-			return parser.text('every 2 minute');
+			return parser.text('every 5 minutes');
 		},
 		job: function() {
 			T.get('search/tweets', {q: query_word, resulttype: "recent"}, function (err, data,response) {
 				if (!err && data.statuses.length > 0) {
 					var tweets = data.statuses;
 					var randomTweet = randIndex(tweets);
-					T.post('favorites/create', {id : randomTweet.id_str},function (err, response) {
+					T.post('favorites/create', {id : randomTweet.id},function (err, response) {
 						if (err && err.code !== 139) console.log('/ query_favorites/create Error: ', err);
 					});
 				}
@@ -40,35 +40,15 @@ if (inProduction()){
 	SyncedCron.add({
 		name: 'Twi_folower_freind',
 		schedule: function(parser) {
-			return parser.text('every 5 minute');
+			return parser.text('every 30 minutes');
 		},
 		job: function() {
-			T.get('followers/ids', function(err, reply) {
-				if(err) {  console.log('followers/ids Error ', err);}
-				else{
-					var followers = reply.ids
-					, randFollower = randIndex(followers);
-
-					T.get('friends/ids', { user_id: randFollower }, function(err, reply) {
-						if(err) { console.log('/ friends/ids Error: ', err); }
-						else{
-							var friends = reply.ids
-							, target  = randIndex(friends);
-
-							T.post('friendships/create', { id: target },  function(err, reply){
-								if(err) { console.log('// friendships/create Error ', err); }
-							});
-						}
-					})
-				}
-			});
-
 			T.get('search/tweets', {q: query_word, resulttype: "recent"}, function (err, reply) {
 			    if(err) console.log('search/tweets and follow Error: ', err)
 
 			    var tweets = reply.statuses;
 
-				if (tweets.length < 1) return false;
+				if (!tweets || tweets.length < 1) return false;
 			    var target = randIndex(tweets).user.id_str;
 
 			    T.post('friendships/create', { id: target },  function(err, reply){
